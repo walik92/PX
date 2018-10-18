af2109e4935352b650310c5b6e4150f2781079da
walik92 <lukaswalkowiak@gmail.com>
2018-10-18 14:21:06 +0200

    add models

---
+++ PX/Models/CompanyModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PX.API.Models
+{
+    public class CompanyModel
+    {
+        [Required] public string Name { get; set; }
+
+        [Required] public string EstablishmentYear { get; set; }
+
+        [Required] public IEnumerable<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ PX/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using PX.DAL.Enums;
+
+namespace PX.API.Models
+{
+    public class EmployeeModel
+    {
+        [Required] public string FirstName { get; set; }
+
+        [Required] public string LastName { get; set; }
+
+        [Required] public DateTime DateOfBirth { get; set; }
+
+        [Required] public JobTitleEnum JobTitle { get; set; }
+    }
+}
